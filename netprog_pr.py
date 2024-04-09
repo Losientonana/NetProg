edd319cf3b1ca9afaf94f9d7808bac79d22fa223
losiento_nana <edcqazs123@naver.com>
2024-04-09 12:37:20 +0900

    04/09 10th

--- netprog_pr.py
+++ netprog_pr.py
@@ -1,7 +1,26 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-numbers = [i for i in range(50)]
-print(numbers)
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
+class Employee(Person):
+    def __init__(self, name, age, employeeID):
+        super().__init__(name, age)  
+        self.employeeID = employeeID
+
+    def getID(self):
+        return self.employeeID
 
 
-squares = [i**2 for i in range(50)]
-print(squares)
+employee = Employee("IoT", 25, 1508)
+
+
+print("이름:", employee.getName())
+print("나이:", employee.getAge())
+print("ID:", employee.getID())
