a65d2408743e2862bb92a0c5f744f20dfbc9174f
losiento_nana <edcqazs123@naver.com>
2024-04-09 12:16:04 +0900

    04/09 9th

--- netprog_pr.py
+++ netprog_pr.py
@@ -1,18 +1,7 @@
 
-def parse_string_to_dict(query_string, delimiter1, delimiter2):
-    result_dict = {}
-    pairs = query_string.split(delimiter1)
-    for pair in pairs:
-        try:
-            key, value = pair.split(delimiter2)
-            result_dict[key] = value
-        except ValueError:
-            print(f"잘못된 형식이 포함되어 있습니다: {pair}")
-            continue
-    return result_dict
+numbers = [i for i in range(50)]
+print(numbers)
 
-query_string = input("쿼리를 입력해주세요: ")
-parsed_dict = parse_string_to_dict(query_string, '&', '=')
 
-print("\n문자열을 딕셔너리로 변환한 결과:")
-print(parsed_dict)
+squares = [i**2 for i in range(50)]
+print(squares)
