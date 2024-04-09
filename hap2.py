58d2b92d77793b0f9e9a1075bde06df1793c498a
losiento_nana <edcqazs123@naver.com>
2024-04-09 11:22:44 +0900

    04/09

--- hap2.py
+++ hap2.py
@@ -1,19 +1,22 @@
-from random import randint
-
-secret_num =  randint(1,100)
-try_count = 0 #시도 횟수
-guess_num = 0 #예상 숫자
-
-while guess_num != secret_num and try_count <= 4:
-    guess_num = eval(input("choosing num"))
-    if(guess_num > secret_num):
-        print("숫자가 더 작습니다.")
-    
-    elif(guess_num < secret_num):
-        print("숫자가 큽니다.")
+class Car :
+    def __init__(self,color,speed):
+        self.color = color
+        self.speed = speed
         
-    else:
-        print("정답입니다.")
+    def speedUp(self,v):
+        self.speed = self.speed + v
+        return self.speed
+    
+    def speedDown(self,v):
+        self.speed = self.speed - v
+        return self.speed
+    
 
-if(guess_num == 5 and secret_num != guess_num):
-    print("ㅅㄱㅇ")
+myCar = Car("black",60)
+print(myCar.color,myCar.speed)
+
+myCar.speedUp(10)
+print(myCar.color,myCar.speed)
+
+myCar.speedDown(200)
+print(myCar.color,myCar.speed)
--- netprog_pr.py
+++ netprog_pr.py
@@ -1,5 +1,5 @@
 a = []
 a.insert(0,"권호윤")
 a.insert(2,"권기현")
-a.append("권데현")
+a.append("권대현")
 print(a)
