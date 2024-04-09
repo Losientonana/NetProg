8bd997ddd061336858b7d3188001d627e663d8d0
losiento_nana <edcqazs123@naver.com>
2024-04-09 12:14:33 +0900

    04/09 8th

--- netprog_pr.py
+++ netprog_pr.py
@@ -1,45 +1,18 @@
-d = [
-    {'name': 'Todd', 'phone': '555-1414', 'email': 'todd@mail.net'},
-    {'name': 'Helga', 'phone': '555-1618', 'email': 'helga@mail.net'},
-    {'name': 'Princess', 'phone': '555-3141', 'email': ''},
-    {'name': 'LJ', 'phone': '555-2718', 'email': 'lj@mail.net'}
-]
-
-
-def print_users_with_phone_ending_in_8():
-    for person in d:
-        if person['phone'].endswith('8'):
-            print(person['name'])
-
-
-def print_users_without_email():
-    for person in d:
-        if not person['email']:
-            print(person['name'])
-
-
-def print_contact_info(name):
-    for person in d:
-        if person['name'] == name:
-            print(f"전화번호: {person['phone']}, 이메일: {person['email']}")
-            return
-    print("이름이 없습니다")
-
 
 def parse_string_to_dict(query_string, delimiter1, delimiter2):
     result_dict = {}
     pairs = query_string.split(delimiter1)
     for pair in pairs:
-        key, value = pair.split(delimiter2)
-        result_dict[key] = value
+        try:
+            key, value = pair.split(delimiter2)
+            result_dict[key] = value
+        except ValueError:
+            print(f"잘못된 형식이 포함되어 있습니다: {pair}")
+            continue
     return result_dict
 
+query_string = input("쿼리를 입력해주세요: ")
+parsed_dict = parse_string_to_dict(query_string, '&', '=')
 
-print("전화번호가 8로 끝나는 사용자:")
-print_users_with_phone_ending_in_8()
-
-print("\n이메일이 없는 사용자:")
-print_users_without_email()
-
-user_input = input("\n사용자 이름을 입력하세요: ")
-print_contact_info(user_input)
+print("\n문자열을 딕셔너리로 변환한 결과:")
+print(parsed_dict)
