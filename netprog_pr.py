ff653a8f7a3be93a7d96013c5d6b57fd3f808c98
losiento_nana <edcqazs123@naver.com>
2024-04-09 11:57:13 +0900

    04/09 5th

--- netprog_pr.py
+++ netprog_pr.py
@@ -1,18 +1,4 @@
-def gcd(a,b):
-    larger = 0
-    smaller = 1000
-    if(a>b):
-        larger = a
-        smaller = b
-    else:
-        larger = b
-        smaller = a
-    while smaller > 0:
-        rem = larger% smaller
-        larger = smaller
-        smaller = rem
-    return larger
-
-if __name__ == "__main__":
-    res =gcd(16,24)
-    print(res)
+word = input("Your word: ")
+a_index = word.find('a') + 1  
+print(word[:a_index])  
+print(word[a_index:]) 
