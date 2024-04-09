1bb77a01a446081263ad8c43b1e18747711fc92c
losiento_nana <edcqazs123@naver.com>
2024-04-09 11:32:14 +0900

    04/09 second

--- netprog_pr.py
+++ netprog_pr.py
@@ -1,5 +1,8 @@
-a = []
-a.insert(0,"권호윤")
-a.insert(2,"권기현")
-a.append("권대현")
-print(a)
+numbers = [1,2,3]
+numbers[1] = 17
+numbers.extend([4,5,6])
+numbers.pop(0)
+numbers.sort()
+numbers.sort(reverse=True)
+numbers.insert(3,25)
+print(numbers)
